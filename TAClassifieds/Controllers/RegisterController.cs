7ee0b160e4e5c0f21124d3577a7e34fba95b3bcf
Shiva Erapatnam <shivae@techaspect.com>
2016-05-03 12:43:15 +0530

    Added EF and Register pages

---
+++ TAClassifieds/Controllers/RegisterController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TAClassifieds.Models;
+
+namespace TAClassifieds.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        //public ActionResult Register()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public ActionResult Register(Register register)
+        //{
+        //    return View();
+        //}
+    }
+}
